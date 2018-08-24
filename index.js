f233af37759161b9eb8e814fe43e5fced73ea79a
james.head@mac.com <james.head@mac.com>
2018-08-24 21:10:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-var a, b
-
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
 
 function add(a,b) {
   
@@ -10,49 +6,32 @@ function add(a,b) {
 }
 
 
-var a, b
-
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
 
 function subtract (a,b) {
   return (a-b)
 }
 
-var a, b
-
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
 
 function multiply(a,b) {
   
  return (a*b)
 }
 
- var a, b
 
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
 
 function divide(a,b) {
   
  return (a/b)
 }
 
- var a, b
 
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
 
 function inc(a) {
   
  return (++a)
 }
 
-var a, b
 
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
 
 function dec(a) {
   
